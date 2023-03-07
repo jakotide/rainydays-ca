276f05663cbb1613f68f03d10fd67e4167f6f6b4
jakotide <111381232+jakotide@users.noreply.github.com>
2023-03-07 21:39:14 +0100

    07.03

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ document.querySelectorAll(".nav-link").forEach(n => n.
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
   }))
+
+
+
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  if(window.scrollY > 0){
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+})
+
+const bars = document.querySelectorAll(".bar");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 0){
+    bars.forEach(bar => bar.classList.add("bar-scrolled"));
+  } else {
+    bars.forEach(bar => bar.classList.remove("bar-scrolled"));
+  }
+});
