2c2610867b6e3316a45973c6d7934479f81b93b6
jakotide <111381232+jakotide@users.noreply.github.com>
2023-03-16 17:26:19 +0100

    16.03

--- js/components/size-boxes.js
+++ js/components/size-boxes.js
@@ -25,9 +25,16 @@ addToCartBtn.addEventListener("click", () => {
     // Change the button to a link and update its href
     const checkoutLink = document.createElement("a");
     checkoutLink.href = "checkout.html";
-    checkoutLink.innerText = "Checkout";
+    checkoutLink.innerText = "CHECKOUT";
+    checkoutLink.style.transition = "background-color .7s ease";
     checkoutLink.classList.add("to-checkout");
     addToCartBtn.parentNode.replaceChild(checkoutLink, addToCartBtn);
+    addToCartBtn.dataset.clicked = "true";
+    setTimeout(() => {
+      checkoutLink.style.backgroundColor = "#FFF";
+      checkoutLink.style.color = "#1f605c";
+    }, 0);
+
     addToCartBtn.dataset.clicked = "true";
   }
 });
