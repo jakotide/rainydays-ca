89c4c081a07bb186c37ada2b61652072a7e494cf
jakotide <111381232+jakotide@users.noreply.github.com>
2023-03-23 16:22:47 +0100

    23.03.23

--- js/components/size-boxes.js
+++ js/components/size-boxes.js
@@ -3,13 +3,11 @@ const boxes = document.querySelectorAll(".size-button");
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
-    // Remove "active" class from all boxes
     boxes.forEach((otherBox) => {
       if (otherBox !== box) {
         otherBox.classList.remove("active");
       }
     });
-    // Toggle "active" class on clicked box
     box.classList.toggle("active");
     localStorage.setItem("selectedSize", box.innerText);
     console.log(`Selected size: ${localStorage.getItem("selectedSize")}`);
@@ -21,10 +19,8 @@ const addToCartBtn = document.querySelector(".cart-button");
 
 addToCartBtn.addEventListener("click", () => {
   if (addToCartBtn.dataset.clicked === "true") {
-    // The button has already been clicked, so redirect to checkout
     window.location.href = "checkout.html";
   } else {
-    // Change the button to a link and update its href
     const checkoutLink = document.createElement("a");
     checkoutLink.href = "checkout.html";
     checkoutLink.innerText = "CHECKOUT";
